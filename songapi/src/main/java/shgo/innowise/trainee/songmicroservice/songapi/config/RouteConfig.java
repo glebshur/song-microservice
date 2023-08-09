437e1e337b61143365ed080f8ed7a9c615d3906f
glebs <glebshurov@gmail.com>
2023-08-09 19:01:31 +0300

    #29 Added rabbitmq and configuration using properties to song api

---
+++ songapi/src/main/java/shgo/innowise/trainee/songmicroservice/songapi/config/RouteConfig.java
@@ -0,0 +1,40 @@
+package shgo.innowise.trainee.songmicroservice.songapi.config;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for camel routes.
+ */
+@Configuration
+public class RouteConfig {
+
+    private final ApplicationContext applicationContext;
+    private final CamelContext camelContext;
+
+    @Autowired
+    public RouteConfig(ApplicationContext applicationContext,
+                       CamelContext camelContext) {
+        this.applicationContext = applicationContext;
+        this.camelContext = camelContext;
+    }
+
+    /**
+     * Registers main consumer route in camel.
+     *
+     * @param qualifier consumer route name from properties.
+     * @return consumer route
+     * @throws Exception error by registering
+     */
+    @Bean
+    public EndpointRouteBuilder mainConsumerRoute(@Value("${main-consumer-route}") String qualifier) throws Exception {
+        EndpointRouteBuilder mainConsumerRoute = applicationContext.getBean(qualifier, EndpointRouteBuilder.class);
+        camelContext.addRoutes(mainConsumerRoute);
+        return mainConsumerRoute;
+    }
+}
--- songapi/src/main/java/shgo/innowise/trainee/songmicroservice/songapi/route/consumer/RabbitmqConsumerRoute.java
+++ songapi/src/main/java/shgo/innowise/trainee/songmicroservice/songapi/route/consumer/RabbitmqConsumerRoute.java
@@ -1,6 +1,5 @@
-package shgo.innowise.trainee.songmicroservice.songapi.route;
+package shgo.innowise.trainee.songmicroservice.songapi.route.consumer;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +9,25 @@ import shgo.innowise.trainee.songmicroservice.songapi.mapper.SongDataDtoMapper;
 import shgo.innowise.trainee.songmicroservice.songapi.service.SongDataService;
 
 /**
- * Camel route that consumes song data from aws sqs.
+ * Camel route that consumes song data from rabbitmq.
  */
-@Component
-@Slf4j
-public class SqsConsumerRoute extends EndpointRouteBuilder {
+@Component("rabbitmq-consumer")
+public class RabbitmqConsumerRoute extends EndpointRouteBuilder {
 
-    private SongDataService songDataService;
-    private SongDataDtoMapper songDataDtoMapper;
+    private final SongDataService songDataService;
+    private final SongDataDtoMapper songDataDtoMapper;
 
     @Autowired
-    public SqsConsumerRoute(SongDataService songDataService,
+    public RabbitmqConsumerRoute(SongDataService songDataService,
                             SongDataDtoMapper songDataDtoMapper) {
         this.songDataService = songDataService;
         this.songDataDtoMapper = songDataDtoMapper;
     }
-
     @Override
     public void configure() {
-        from(aws2Sqs("{{consumer-queue}}").deleteAfterRead(true).delay("{{delay:1000}}"))
+        from(springRabbitmq("{{message-brokers.rabbitmq.exchange}}")
+                .queues("{{message-brokers.rabbitmq.consumer-queue}}")
+                .routingKey("{{message-brokers.rabbitmq.consumer-queue}}"))
                 .unmarshal().json(JsonLibrary.Jackson, SongDataDto.class)
                 .process(exchange -> {
                     SongDataDto body = exchange.getMessage().getBody(SongDataDto.class);
